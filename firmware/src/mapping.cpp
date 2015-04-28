18aaa7282cd47ce7170edf667a0abe3a036da18d
Gregwar <g.passault@gmail.com>
2015-04-28 15:11:32 +0200

    Fixing remap memory

--- firmware/src/mapping.cpp
+++ firmware/src/mapping.cpp
@@ -33,6 +33,7 @@ void colorize()
  */
 void remap(int direction)
 {
+    currentMapping = direction;
     for (int i=0; i<12; i++) {
         mapping[i] = servos_order[(i+3*direction)%12];
     }
@@ -47,7 +48,6 @@ TERMINAL_COMMAND(remap,
         terminal_io()->print("remap=");
         terminal_io()->println(currentMapping);
     } else {
-        currentMapping = atoi(argv[0]);
         remap(currentMapping);
     }
 }
