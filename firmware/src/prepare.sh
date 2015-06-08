565f10d68bfe5e52a937c0ffb35035bf30e0dfbb
Gregwar <g.passault@gmail.com>
2015-06-08 11:02:58 +0200

    Adding rhock to prepare

--- firmware/src/prepare.sh
+++ firmware/src/prepare.sh
@@ -3,6 +3,10 @@
 if [ -d "RobotsWar" ]; then
     cd RobotsWar &&
     git pull
+    cd ../Rhock &&
+    git pull
 else
     git clone https://github.com/RobotsWar/RobotsWar.git RobotsWar --depth=1
+    git clone https://github.com/RobotsWar/RobotsWar.git Rhock --depth=1
+    ln -s $PWD/Rhock RobotsWar/Rhock
 fi
