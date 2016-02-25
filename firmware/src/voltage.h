5d82049a7f8d536d0f31cec74a9ef8100d39960f
Gregwar <g.passault@gmail.com>
2016-02-25 11:47:21 +0100

    More permissive shut

--- firmware/src/voltage.h
+++ firmware/src/voltage.h
@@ -5,7 +5,7 @@
 #define VOLTAGE_R1      20
 #define VOLTAGE_R2      10
 #define VOLTAGE_LIMIT   6.9
-#define VOLTAGE_SHUT    5
+#define VOLTAGE_SHUT    5.3
 
 void voltage_init();
 void voltage_tick();
