a93a7d28228134ccb7c0f35cbfdbd9dfb2bda04f
Gregwar <g.passault@gmail.com>
2016-02-25 11:46:47 +0100

    One distance sensor

--- firmware/src/distance.cpp
+++ firmware/src/distance.cpp
@@ -3,34 +3,25 @@
 #include <wirish/wirish.h>
 #include "distance.h"
 
-int distance1, distance2, distance3, distance4;
+#define DISTANCE_PIN     3
 
-#define DISTANCE_P1     3
-#define DISTANCE_P2     4
-#define DISTANCE_P3     5
-#define DISTANCE_P4     6
+static int distance;
 
 void distance_init()
 {
-    pinMode(DISTANCE_P1, INPUT);
-    pinMode(DISTANCE_P2, INPUT);
-    pinMode(DISTANCE_P3, INPUT);
-    pinMode(DISTANCE_P4, INPUT);
+    pinMode(DISTANCE_PIN, INPUT);
 }
 
-void distance_update()
+int distance_get()
 {
-    distance1 = analogRead(DISTANCE_P1);
-    distance2 = analogRead(DISTANCE_P2);
-    distance3 = analogRead(DISTANCE_P3);
-    distance4 = analogRead(DISTANCE_P4);
+    // XXX: Convert voltage to distance
+    return analogRead(DISTANCE_PIN);
 }
 
 TERMINAL_COMMAND(dist, "Monitor distances")
 {
     while (!SerialUSB.available()) {
-        distance_update();
-        terminal_io()->println(distance1);
+        terminal_io()->println(distance_get());
         delay(10);
     }
 }
